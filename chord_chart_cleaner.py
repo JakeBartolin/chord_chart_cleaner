f5a900969b86847d094c5c217d5158f6e697c02f
JakeBartolin <105478928+JakeBartolin@users.noreply.github.com>
2023-02-02 18:24:18 -0500

    fork from TFT
    - remove workoutdata cleanup code
    - refactor function and variable names to follow PEP 8
    - remove useless if statement (line 100-107)

--- chord_chart_cleaner.py
+++ chord_chart_cleaner.py
@@ -1,41 +1,21 @@
 import re
 import sys
 
-def Main():
-    userInput = ""
-    isActive = True
-    welcomeMessage = """Starting TFT Script...
+def print_welcome_message():
+    welcome_message = """Starting TFT Script...
 
 ********************
 * Text Format Tool *
-******************** 
+********************
 
 """
-    functionSelect = """Please select a function:
-1) Format Workout Data
-2) Formate Chord Chart and Lyrics
-x) Exit
+    print(welcome_message)
+    
+    
+def main():
+    return
 
-"""
-
-    print(welcomeMessage)
-
-    while isActive == True:
-        print(functionSelect)
-        userInput = input()
-
-        if userInput == "1":
-            FormatWorkoutData()
-        elif userInput == "2":
-            FormatChordChartAndLyrics()
-        elif userInput == "x":
-            isActive = False
-        else:
-            print("Sorry, not a valid entry, please try again.\n\n")
-
-    print("Exiting Script....")
-
-def GetText():
+def get_text():
     
     print("Please paste your data below using Ctrl+Shift+V.\nWhen you are done, press Ctrl+D for Linux/Mac or Ctrl+Z for Windows.\n")
     lines = []
@@ -45,27 +25,18 @@ def GetText():
         pass
     return lines
 
-def FormatWorkoutData():
-    print("Starting Format Workout Data Subscript...\n")
-    text = GetText()
-    text = ProcessWorkoutData(text)
-   
-    print("\n\nYour formatted data is printed below:\n\n")
-    print(f"|Excercise|Sets/Reps|Weight|Reserve Reps|Notes|\n|---|---|---|---|---|{text}\n\n")
 
-    return
-
-def FormatChordChartAndLyrics():
+def format_chord_chart_and_lyrics():
     print("Starting Extract Chords and Lyrics Subscript...\n")
-    text = GetText()
+    text = get_text()
 
     print("********************************\n*Your chords are printed below:*\n********************************\n")
-    print(ExtractChordChart(text) + "\n\n")
+    print(extract_chord_chart(text) + "\n\n")
 
     print("********************************\n*Your lyrics are printed below:*\n********************************\n")
-    print(ExtractLyrics(text))
+    print(extract_lyrics(text))
 
-def ExtractChordChart(text):
+def extract_chord_chart(text):
     output = ""
 
     for line in text:
@@ -76,11 +47,11 @@ def ExtractChordChart(text):
             
     return output
 
-def ExtractLyrics(text):
+def extract_lyrics(text):
     output = ""
 
     for line in text:
-        spaceCount = 0
+        space_count = 0
 
         if line == "\n":
             continue
@@ -90,57 +61,21 @@ def ExtractLyrics(text):
         
         for character in line:
             if character == " ":
-                spaceCount += 1
-        if spaceCount > (len(line) / 3):
+                space_count += 1
+        if space_count > (len(line) / 3):
             continue
 
         if line == "[Intro]":
             continue
 
-        if line.__contains__("[Chorus]"):
-            line = line.replace("[Chorus]", "## Chorus")
-        elif line.__contains__("[Verse]"):
-            line = line.replace("[Verse]", "## Verse")
-        elif line.__contains__("[Bridge]"):
-            line = line.replace("[Bridge]", "## Bridge")
-        elif line.__contains__("[Solo]"):
-            line = line.replace("[Solo]", "## Solo")
+        line = line.replace("[Chorus]", "## Chorus")
+        line = line.replace("[Verse]", "## Verse")
+        line = line.replace("[Bridge]", "## Bridge")
+        line = line.replace("[Solo]", "## Solo")
         
         output += line
     
     print(output)
     return
 
-def ProcessWorkoutData(data):
-
-    isFirstExcercise = True
-    output = ""
-
-    for line in data:
-
-        line = re.sub('\n', '', line)
-
-        if (line == ''):
-            continue
-        elif (line.startswith("N:0")):
-            line = 'xxN/A'
-        elif (line.startswith("E:")):
-            line = 'xx|' + line[2:]
-            if (isFirstExcercise == True):
-                isFirstExcercise = False
-            else:
-                line = 'xx\n' + line[2:]
-        elif (line.startswith("N:")):
-            line = re.sub('tt- ', '<br>- ', line)
-
-        output += line[2:]
-
-        if (output.endswith('|') == False):
-            output += '|'
-    return output
-
-def PrintWorkoutData(data):
-    print("\n\nYour formatted data is printed below:\n\n")
-    print(f"|Excercise|Sets/Reps|Weight|Reserve Reps|Notes|\n|---|---|---|---|---|{data}\n\n")
-
-Main()
+main()
